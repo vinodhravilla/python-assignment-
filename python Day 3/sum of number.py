7bb73051b54f959d01c5447d13cbeeb583eb0e59
vinodhravilla <31848702+vinodhravilla@users.noreply.github.com>
2020-07-23 08:19:50 +0530

    Add files via upload

---
+++ python Day 3/sum of number.py
@@ -0,0 +1,12 @@
+natural_Number = int(input("Enter a natural number: \n"))
+# I have stored this to display the actual number !!
+temp = natural_Number
+sum = 0
+condition = 0
+
+while (condition < natural_Number):
+    sum += natural_Number
+    natural_Number -= 1
+    print(natural_Number , "-->" , sum)
+
+print(f"\nSum of natural numbers for the number {temp} is found out to be : {sum}")
